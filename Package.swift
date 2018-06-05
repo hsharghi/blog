41a0e9fd5078b1a5a3a96d0a38d72abeb3955eb6
Hadi Sharghi <hsharghi@gmail.com>
2018-06-05 19:31:20 +0430

    add Fakery package

--- Package.swift
+++ Package.swift
@@ -12,12 +12,12 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc.4.1"),
         .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc.2.1"),
-        .package(url: "https://github.com/skelpo/JWTMiddleware.git", from: "0.7.0")
-
+        .package(url: "https://github.com/skelpo/JWTMiddleware.git", from: "0.7.0"),
+        .package(url: "https://github.com/hsharghi/Fakery.git", from: "3.4.1")
     ],
     targets: [
         //            .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
-        .target(name: "App", dependencies: ["FluentMySQL", "Vapor", "Authentication", "JWT", "JWTMiddleware"]),
+        .target(name: "App", dependencies: ["FluentMySQL", "Vapor", "Authentication", "JWT", "JWTMiddleware", "Fakery"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
