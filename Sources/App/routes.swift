d1617b02bb495b415104fd1c137f98ac33c26304
Hadi Sharghi <hsharghi@gmail.com>
2018-06-05 20:16:40 +0430

    add SeederController to routes cleanup code

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,19 +2,23 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "Hello, world!" example
+    // Basic "Hello, world!" example to check if local server is working
     router.get("hello") { req in
         return "Hello, world!"
     }
-
+    
     try router.register(collection: UserController())
     try router.register(collection: PostController())
+    try router.register(collection: CommentController())
+    try router.register(collection: SeederController())
+    
 
     // Example of configuring a controller
-//    let userController = UserController()
-//    router.post("users", use: UserController.create)
-//    router.get("users", use: UserController.index)
-//    router.get("users", User.parameter, use: UserController.show)
-//    router.patch("users", use: UserController.update)
-//    router.delete("users", User.parameter, use: UserController.delete)
+    //    let userController = UserController()
+    //    router.post("users", use: UserController.create)
+    //    router.get("users", use: UserController.index)
+    //    router.get("users", User.parameter, use: UserController.show)
+    //    router.patch("users", use: UserController.update)
+    //    router.delete("users", User.parameter, use: UserController.delete)
 }
+
