6db68673694cc33eaded3c1bc3aae219e631fad5
Hadi Sharghi <hsharghi@gmail.com>
2018-09-13 18:28:59 +0430

    remove jwt 

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,7 +2,6 @@
 import FluentMySQL
 import Vapor
 import Authentication
-import JWTMiddleware
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
